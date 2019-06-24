20e45df9da07386a703c2f352316b0b74749b1d9
M Banks <montensia1@outlook.com>
2019-06-24 21:35:26 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_season_day_to_show_table.rb
+++ db/migrate/008_add_season_day_to_show_table.rb
@@ -1,6 +1,6 @@
 class AddSeasonDayToShowTable < ActiveRecord::Migration[4.2]
   def change
-    add_column :show, :season, :integer 
-    add_column :show, :channel, :integer
+    add_column :show, :season, :string
+    add_column :show, :day, :integer
   end
 end
